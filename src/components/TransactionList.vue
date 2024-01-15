512399d1546236beebe35bc6189aae744795a145
Camilla Westin <camilla@webstin.se>
2024-01-15 14:34:21 +0100

    Added some dummy data to list

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,3 +1,10 @@
+<script setup>
+const transactions = [
+  { id: 1, text: "Flower", amount: -19.99 },
+  { id: 2, text: "Salary", amount: 219.95 },
+  { id: 2, text: "Food", amount: -20.95 },
+];
+</script>
 <template>
   <div class="my-3">
     <h2 class="font-semibold text-sm">History</h2>
@@ -5,16 +12,12 @@
 
     <ul id="list">
       <li
-        class="bg-white p-2 my-1 text-xs border-r-2 border-red-600 flex justify-between"
+        class="bg-white p-2 my-1 text-xs border-r-2 flex justify-between"
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :class="transaction.amount < 0 ? 'border-red-600' : 'border-green-600'"
       >
-        Cash <span>-$300</span>
-        <button class="hidden">X</button>
-      </li>
-      <li
-        class="bg-white p-2 my-1 text-xs border-r-2 border-green-600 flex justify-between"
-      >
-        Salary <span>$300</span>
-        <button class="hidden">X</button>
+        {{ transaction.text }} <span>${{ transaction.amount }}</span>
       </li>
     </ul>
   </div>
