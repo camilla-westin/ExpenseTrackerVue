d1dae7f3d7e6c6dd6291255bd1f30f00ee2f174b
Camilla Westin <camilla@webstin.se>
2024-01-15 14:45:22 +0100

    Added total amount to balance component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import Balance from "@/components/Balance.vue";
 import IncomeExpenses from "@/components/IncomeExpenses.vue";
 import TransactionsList from "@/components/TransactionList.vue";
@@ -10,13 +10,19 @@ const transactions = ref([
   { id: 2, text: "Salary", amount: 219.95 },
   { id: 2, text: "Food", amount: -20.95 },
 ]);
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
 </script>
 
 <template>
   <main class="flex justify-center items-center h-screen bg-nice-green">
     <div>
       <h1 class="font-semibold text-m">Expense tracker</h1>
-      <Balance />
+      <Balance :total="total" />
       <IncomeExpenses />
       <TransactionsList :transactions="transactions" />
       <AddTransaction />
--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,6 +1,17 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
 <template>
-  <div class="py-2">
+  <div class="py-3">
     <h2 class="uppercase text-xs font-medium">Your balance</h2>
-    <p class="text-xl font-semibold">0.00$</p>
+    <p class="text-xl font-semibold">{{ total }}$</p>
   </div>
 </template>
